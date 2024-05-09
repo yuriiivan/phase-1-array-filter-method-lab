02197b759250ceb11a0c5d8e2d89ce011313077d
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-09 10:30:42 -0400

    Add fuzzy word search capability

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const findMatching = (drivers,name) => {
   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
 }
+
+const fuzzyMatch = (drivers, name) => {
+  return drivers.filter(driver => driver.substring(0,1) === name.substring(0,1))
+}
