09dbe55fceba8d9b28742513f59dd34eacefe879
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-09 10:37:15 -0400

    Add func to match names

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// Code your solution here
 const findMatching = (drivers,name) => {
-  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
 }
 
 const fuzzyMatch = (drivers, name) => {
-  return drivers.filter(driver => driver.substring(0,1) === name.substring(0,1))
+  return drivers.filter(driver => driver.substring(0,1) === name.substring(0,1));
+}
+
+const matchName = (drivers, name) => {
+  return drivers.filter(driver => driver.name === name);
 }
